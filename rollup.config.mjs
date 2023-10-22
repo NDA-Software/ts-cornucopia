fd04ad04d260ab40e3cd71bae4fb23efe440378e
Herr Turing <admin@nda.software>
2023-10-21 21:12:32 -0300

    Changes to work with new patterns defined while coding create-ts-cornucopia.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,73 +1,191 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import commonjs from '@rollup/plugin-commonjs';
-import nodeResolve from '@rollup/plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
-import dts from 'rollup-plugin-dts';
-import del from 'rollup-plugin-delete';
+import { existsSync, mkdirSync, writeFileSync, readFileSync } from "fs";
+
+import nodeResolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
+
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import copy from "rollup-plugin-copy";
+import del from "rollup-plugin-delete";
 import indexer from "rollup-plugin-indexer";
 
-export default [{
-    input: 'src/index.ts',
-    output: [{
-        exports: 'named',
-        preserveModules: true,
-        interop: 'auto',
-        dir: '.build/',
-        format: 'esm'
-    }],
+const packageJson = JSON.parse(readFileSync("./package.json"));
+const tsConfig = JSON.parse(readFileSync("./tsconfig.json"));
+
+const applyCustomization = (defaultConfig, customConfig) => {
+    let output = defaultConfig.output;
+
+    if (customConfig.output && customConfig.output[0]) {
+        output[0] = {
+            ...output[0],
+            ...customConfig.output[0]
+        };
+
+        if (customConfig.output[1]) {
+            customConfig.output.shift();
+
+            output = [
+                ...output,
+                ...customConfig.output
+            ];
+        }
+    }
+
+    const plugins = [
+        ...sharedConfigs.plugins,
+        ...customConfig.plugins,
+        ...defaultConfig.plugins
+    ];
+
+    return {
+        ...sharedConfigs,
+        ...customConfig,
+        output,
+        plugins,
+    };
+}
+
+// #region Mock Data:
+const tempFolder = ".temp"
+if (!existsSync(tempFolder))
+    mkdirSync(tempFolder);
+
+const mockFile = `${tempFolder}/mock.js`;
+if (!existsSync(mockFile))
+    writeFileSync(mockFile, "export default () => 42;");
+
+const mockConfig = {
+    input: ".temp/mock.js",
+    output: [{ dir: ".temp/", }],
+};
+// #endregion
+
+// #region Base Configs:
+const { exports } = packageJson;
+
+const config = [];
+
+const sharedConfigs = {
+    input: "src/index.ts",
+    plugins: [
+        nodeResolve(),
+        peerDepsExternal(),
+        json(),
+    ],
+};
+
+const hasCjs = exports["."].require;
+const hasEsm = exports["."].import;
+const hasTypes = !!tsConfig.compilerOptions.declaration;
+
+let declaration = hasTypes;
+let declarationDir = declaration ? "./dist/types/" : undefined;
+const preserveModules = (Object.keys(exports)).length > 1;
+// #endregion
+
+// #region Customizations:
+const configCjs = {
+    output: [{}],
     plugins: [
         indexer("./src", { exportMode: "default" }),
         indexer(["./src/file", "./src/number", "./src/scripts", "./src/string"]),
-        typescript({
-            tsconfig: 'tsconfig.json',
-            outDir: '.build/',
-            include: [
-                './src/**/*.ts'
-            ]
-        }),
-        peerDepsExternal(),
-        nodeResolve(),
-        commonjs()
     ]
-},
-{
-    input: '.build/src/index.d.ts',
-    output: [{ file: '.build/index.d.ts', format: 'esm' }],
-    plugins: [dts()]
-},
-{
-    input: '.build/src/file/index.d.ts',
-    output: [{ file: '.build/file/index.d.ts', format: 'esm' }],
-    plugins: [dts()]
-},
-{
-    input: '.build/src/number/index.d.ts',
-    output: [{ file: '.build/number/index.d.ts', format: 'esm' }],
-    plugins: [dts()]
-},
-{
-    input: '.build/src/scripts/index.d.ts',
-    output: [{ file: '.build/scripts/index.d.ts', format: 'esm' }],
-    plugins: [dts()]
-},
-{
-    input: '.build/src/string/index.d.ts',
-    output: [{ file: '.build/string/index.d.ts', format: 'esm' }],
-    plugins: [dts()]
-},
-{
-    input: './.build/index.js',
-    output: {
-        file: './.temp/dummy.txt'
-    },
-    plugins: [
-        del({ targets: '.build/src' })
-    ],
-    onwarn(warning, warn) {
-        if (warning.code === 'UNRESOLVED_IMPORT')
-            return false;
+};
 
-        warn(warning);
-    }
+const configEsm = {
+    output: [{}],
+    plugins: []
+};
+// #endregion
+
+// #region Preparing Export Data:
+if (hasCjs) {
+    const finalConfigCjs = applyCustomization({
+        output: [{
+            exports: "named",
+            dir: "dist/",
+            format: "cjs",
+            preserveModules,
+            entryFileNames: "[name].cjs"
+        }],
+        plugins: [
+            typescript({ tsconfig: "tsconfig.json", declaration, declarationDir }),
+        ],
+    }, configCjs);
+
+    config.push(finalConfigCjs);
 }
-];
+
+if (hasEsm) {
+    let folder = "dist/"
+
+    if (hasCjs) {
+        folder = "dist/esm/"
+        declaration = false;
+        declarationDir = undefined;
+
+        config.push({
+            ...mockConfig,
+            plugins: [
+                copy({
+                    targets: [
+                        { src: ['dist/*', '!dist/cjs', '!dist/types', '!dist/esm'], dest: "dist/cjs/" }
+                    ]
+                })
+            ],
+        });
+
+        config.push({
+            ...mockConfig,
+            plugins: [
+                del({
+                    targets: ["dist/*", "!dist/cjs", "!dist/types"],
+                    recursive: true
+                })
+            ],
+        });
+    }
+
+    const finalConfigEsm = applyCustomization({
+        output: [{
+            exports: "named",
+            dir: folder,
+            format: "esm",
+            preserveModules,
+            entryFileNames: "[name].mjs"
+        }],
+        plugins: [
+            typescript({ tsconfig: "tsconfig.json", declaration, declarationDir })
+        ],
+    }, configEsm);
+
+    config.push(finalConfigEsm);
+}
+// #endregion
+
+// #region Organizing Types:
+if (hasTypes) {
+    config.push({
+        ...mockConfig,
+        plugins: [
+            copy({
+                targets: [
+                    { src: "dist/types/src/*", dest: "dist/types" }
+                ]
+            })
+        ],
+    });
+
+    config.push({
+        ...mockConfig,
+        plugins: [
+            del({
+                targets: ["dist/types/src"],
+                recursive: true
+            })
+        ],
+    });
+}
+// #endregion
+
+export default config;
