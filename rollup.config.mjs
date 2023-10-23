5e24e9461838b037d2a8fa918b03e406d667b51c
Herr Turing <admin@nda.software>
2023-10-23 18:07:09 -0300

    Fixed type declarations.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -182,7 +182,7 @@ if (hasTypes) {
         ...mockConfig,
         plugins: [
             del({
-                targets: ['dist/types/src'],
+                targets: ['dist/types/src', 'dist/types/tests'],
                 recursive: true
             })
         ]
